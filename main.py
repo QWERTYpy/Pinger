6d6e1e9800fba59eea9d4efcba4559399d416b25
QWERTYpy <burtsevmv@mail.ru>
2022-04-11 15:01:24 +0300

    Исправление циклов

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+"""
+Скрипт для опроса сетевых устройств
+"""
+import openpyxl  # Библиотека для работы с Excel
+from prettytable import PrettyTable
+from colorama import init
+from colorama import Fore, Back, Style
+from ping3 import ping, verbose_ping
+from tqdm import tqdm
+from tkinter import messagebox
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ip_book = openpyxl.load_workbook("ip cam.xlsx")  # Открывает файл
+worksheet = ip_book.active  # Делаем его активным
+max_row = worksheet.max_row  # Получаем максимальное количество строк
+max_col = worksheet.max_column  # Получаем максимальное количество столбцов
 
+# for row in range (1, max_row):
+#     if
+# print(worksheet.max_row, worksheet.max_column)
+# for i in range(0, worksheet.max_row):
+# for col in worksheet.iter_cols(1, worksheet.max_column):
+#     print(col[2].value, end="\t\t")
+#     print('')
+# print(worksheet.cell(row=1,column=5).value)
+# for row in range(1, max_row):
+#     val = worksheet.cell(row=row, column=5).value
+#     if val == 'Камера':
+#         print(worksheet.cell(row=row, column=1).value)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+init()
+table_ip = PrettyTable()
+table_ip.field_names = ['IP Адрес', 'Статус', 'Комментарий']
+scan_ip=[]
+off_ip=[]
+count_device = 0
+for row in range(1, max_row):  # Запускаем цикл по всем строкам
+    val = worksheet.cell(row=row, column=5).value
+    if val == 'Камера':  # Если устройство камера
 
+        count_device+=1
+        ip_cam = worksheet.cell(row=row, column=1).value
+        comment = worksheet.cell(row=row, column=6).value
+        ip_pin = ping(ip_cam)
+        if ip_pin is None:
+            ip_pin = ping(ip_cam)
+            if ip_pin is None:
+                off_ip.append([ip_cam, row, comment])
+                table_ip.add_row([Fore.RED+ip_cam+Style.RESET_ALL, ip_pin, comment])
+print('Всего устройств : ' + str(count_device) + Fore.GREEN + '  На связи : ' + str(count_device-len(off_ip)) +
+      Fore.RED + '  Отсутсвуют : ' + str(len(off_ip)) + Style.RESET_ALL)
+print(table_ip)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import win10toast
+toaster = win10toast.ToastNotifier()
+error_text = ''
+for ip, _, com in off_ip:
+    error_text +=f'{ip} - {com}\n'
+toaster.show_toast("Отсутсвуют", error_text, duration=100)
+#messagebox.showwarning('Отсутствуют', off_ip)
+# print('Проверка недоступных устройств')
+# for ip, row in off_ip:
+#     print(Fore.RED+ip+Style.RESET_ALL)
+#     verbose_ping(ip)
