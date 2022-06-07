7441fcf4a47c788f6b7bc365ae8f2557de0dc3d4
QWERTYpy <burtsevmv@mail.ru>
2022-06-07 16:32:18 +0300

    Добавлена возможность отключения фонового пинга. Задалбывает при неисправности сети

--- pinger.py
+++ pinger.py
@@ -27,17 +27,21 @@ def print_time_input(timeout):
     start = time.monotonic()
     now = time.monotonic()
     count_sec = 0
+    global hidden_ping
     while now - start < timeout:
         if now - start > count_sec:
             count_sec += 1
             print_time = timeout-count_sec
             delta = str(datetime.now()-start_programs)[:-7]
-            print("\r%s Осталось %03d c. | u-обновить | t-таймер | q-выход :" % (delta, print_time), end='')
+            hping = 'ON' if hidden_ping else 'OFF'
+            print("\r%s Осталось %03d c. | h - hping %s | u-обновить | t-таймер | q-выход :" % (delta, print_time, hping), end='')
         if msvcrt.kbhit():
             c = msvcrt.getwch()
             if c == 'q' or c == 'й':
                 lists.reboot_ping = True
                 sys.exit()
+            if c == 'h' or c == 'р':
+                hidden_ping = not hidden_ping
             if c == 'u' or c == 'г':
                 break
             if c == 't' or c == 'е':
@@ -48,9 +52,6 @@ def print_time_input(timeout):
         now = time.monotonic()
 
 
-lists = ListsIP()
-console = Console()
-
 def progress_bar():
     with Progress() as progress:
         task_ping = progress.add_task("[red]Опрос устройств...", total=lists.progressbar)
@@ -58,7 +59,9 @@ def progress_bar():
             progress.update(task_ping, completed=lists.progressbar_complit)
 
 
-
+lists = ListsIP()
+console = Console()
+hidden_ping = True
 start_programs = datetime.now()
 while True:
     tables = TableGeneration()
@@ -69,6 +72,7 @@ while True:
     with concurrent.futures.ThreadPoolExecutor(max_workers=dict_const['ping_device']) as executor:
         executor.map(lists.thread_ping, lists.scan_ip)
 
+    lists.progressbar_complit = lists.progressbar # После сборки в exe наблюдается глюк. Иногда. Это костыль для завершения Опроса
         # while not progress.finished:
         #     progress.update(task_ping, completed=lists.progressbar_complit)
     tables.table_filling(lists.scan_ip, lists.modification_off_ip)
@@ -83,7 +87,7 @@ while True:
     os.system(f"mode con:cols={dict_const['console'][0]} lines={dict_const['console'][1]}")
     console.print(tables.grid_main)
 
-    if len(lists.off_ip) > 0:
+    if len(lists.off_ip) > 0 and hidden_ping:
         threading.Thread(target=lists.hidden_ping, args=(lists.off_ip,)).start()
     print_time_input(dict_const['time_sec'])
     os.system('cls')
